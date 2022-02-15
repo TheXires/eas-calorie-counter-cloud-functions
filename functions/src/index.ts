91b553234b911aed076c86fdce1fdbe8a2e0cd3e
TheXires <xires.dev@gmail.com>
2022-02-15 17:05:49 +0100

    changed function name and removed some logs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -47,88 +47,90 @@ export const deleteUser = functions.https.onCall(async (_, context) => {
 /**
  * creates and updates statistics based on user consumptions
  */
-export const createStatistics = functions.https.onCall(async (_, context) => {
-  if (!context.auth) {
-    throw new functions.https.HttpsError(
-      'permission-denied',
-      'Unauthorized request. Login to create statistics.',
-    );
-  }
-
-  const userId: string = context.auth.uid;
-  console.log(`creating statistics for ${userId}`);
-
-  const statisticsDocument = await db
-    .collection('users')
-    .doc(userId)
-    .collection('statistics')
-    .doc('dailyStatistics')
-    .get();
-
-  const statisticsData = statisticsDocument.data()?.data ?? [];
-  const lastModified: number = statisticsDocument.data()?.lastModified ?? 0;
-
-  const modifiedConsumptionDocuments = await db
-    .collection('users')
-    .doc(userId)
-    .collection('consumptions')
-    .where('lastModified', '>=', lastModified)
-    .get();
-
-  if (modifiedConsumptionDocuments.docs.length <= 0) {
-    return {
-      updatedStatistics: false,
-    };
-  }
-
-  modifiedConsumptionDocuments.docs.forEach((doc) => {
-    const statisticValues = {
-      date: doc.data().date,
-      calories: 0,
-      carbohydrates: 0,
-      fat: 0,
-      protein: 0,
-    };
-
-    if (doc.data().deleted != null && !doc.data().deleted) {
-      doc.data().items.forEach((item: any) => {
-        statisticValues.calories += item.calories * item.quantity;
-        statisticValues.carbohydrates += item.carbohydrates * item.quantity;
-        statisticValues.fat += item.fat * item.quantity;
-        statisticValues.protein += item.protein * item.quantity;
-      });
-
-      const index = statisticsData.findIndex(
-        (element: any) => element.date === doc.data().date,
+export const createDailyStatistics = functions.https.onCall(
+  async (_, context) => {
+    if (!context.auth) {
+      throw new functions.https.HttpsError(
+        'permission-denied',
+        'Unauthorized request. Login to create statistics.',
       );
-
-      if (index === -1) {
-        statisticsData.push(statisticValues);
-      } else {
-        statisticsData[index] = statisticValues;
-      }
-    } else {
-      const index = statisticsData.findIndex(
-        (element: any) => element.date === doc.data().date,
-      );
-      if (index !== -1) statisticsData.splice(index, 1);
     }
-  });
 
-  await firebase
-    .firestore()
-    .collection('users')
-    .doc(userId)
-    .collection('statistics')
-    .doc('dailyStatistics')
-    .set({
-      lastModified: Date.now(),
-      data: statisticsData,
+    const userId: string = context.auth.uid;
+    console.log(`creating statistics for ${userId}`);
+
+    const statisticsDocument = await db
+      .collection('users')
+      .doc(userId)
+      .collection('statistics')
+      .doc('dailyStatistics')
+      .get();
+
+    const statisticsData = statisticsDocument.data()?.data ?? [];
+    const lastModified: number = statisticsDocument.data()?.lastModified ?? 0;
+
+    const modifiedConsumptionDocuments = await db
+      .collection('users')
+      .doc(userId)
+      .collection('consumptions')
+      .where('lastModified', '>=', lastModified)
+      .get();
+
+    if (modifiedConsumptionDocuments.docs.length <= 0) {
+      return {
+        updatedStatistics: false,
+      };
+    }
+
+    modifiedConsumptionDocuments.docs.forEach((doc) => {
+      const statisticValues = {
+        date: doc.data().date,
+        calories: 0,
+        carbohydrates: 0,
+        fat: 0,
+        protein: 0,
+      };
+
+      if (doc.data().deleted != null && !doc.data().deleted) {
+        doc.data().items.forEach((item: any) => {
+          statisticValues.calories += item.calories * item.quantity;
+          statisticValues.carbohydrates += item.carbohydrates * item.quantity;
+          statisticValues.fat += item.fat * item.quantity;
+          statisticValues.protein += item.protein * item.quantity;
+        });
+
+        const index = statisticsData.findIndex(
+          (element: any) => element.date === doc.data().date,
+        );
+
+        if (index === -1) {
+          statisticsData.push(statisticValues);
+        } else {
+          statisticsData[index] = statisticValues;
+        }
+      } else {
+        const index = statisticsData.findIndex(
+          (element: any) => element.date === doc.data().date,
+        );
+        if (index !== -1) statisticsData.splice(index, 1);
+      }
     });
 
-  console.log(`finished creating statistics for ${userId}`);
+    await firebase
+      .firestore()
+      .collection('users')
+      .doc(userId)
+      .collection('statistics')
+      .doc('dailyStatistics')
+      .set({
+        lastModified: Date.now(),
+        data: statisticsData,
+      });
 
-  return {
-    updatedStatistics: true,
-  };
-});
+    console.log(`finished creating statistics for ${userId}`);
+
+    return {
+      updatedStatistics: true,
+    };
+  },
+);
